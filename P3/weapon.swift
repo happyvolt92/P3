1ac43bb94959c4aaeee90c5106b6b7634c653aa0
elodie <elodie@ca-rool.com>
2022-05-12 11:16:18 +0200

    weapon diet

--- P3/weapon.swift
+++ P3/weapon.swift
@@ -5,24 +5,14 @@
 //  Created by Elodie GAGE on 14/04/2022.
 //
 
-import Foundation
-
 class Weapon {
-
-    let weaponName: String
-    let damage: Int
-
-    let sword = Weapon(weaponName: String, damage: 30),
-    let staff = Weapon(weaponName: String, name: "staff", damage: 10, heal: 40),
-    let axe = Weapon(weaponName: String, damage: 50),
-    let ironFist = Weapon(weaponName: String, damage: 20)
+    var name = ""
+    var damage = 0
     
-    
-    init(weaponName: String, damage: Int) {
-        self.weaponName = weaponName
-        self.damage = damage
-        
+    init(damage: Int) {
+            self.damage = damage
+        }
     }
     
-
-}
+    
+ 
