eeaf8680ab0d8affc6a2b51e8de3f996712c94fe
elodie <elodie@ca-rool.com>
2022-05-12 16:32:58 +0200

    indentation

--- P3/characters.swift
+++ P3/characters.swift
@@ -5,20 +5,20 @@ class Character {
     let type: CharacterType
     
     
-    init(name: String, type: CharacterType) {
-        self.name = name
-        // weapon enum
-        self.weapon = type.weapon()
-        //life is now combinate with life
-        self.life = type.life()
-        self.type = type
-    }
+        init(name: String, type: CharacterType) {
+            self.name = name
+            // weapon enum
+            self.weapon = type.weapon()
+            //life is now combinate with life
+            self.life = type.life()
+            self.type = type
+        }
     
         func attack(character: Character){
             character.life = character.life - self.weapon.damage
             print("\(self.name) damaged \(character.name)by \(self.weapon.damage) !")
        
-    }
+        }
     
         // so nothing else can be choose
         enum CharacterType: String {
@@ -27,26 +27,25 @@ class Character {
                 case colossus = "Colossus"
                 case dwarf = "Dwarf"
     
-        // func to sync characters's life
-        func life() -> Int {
-            switch self {
-                case .warrior: return 600
-                case .mage: return 800
-                case .colossus: return 900
-                case .dwarf: return 400
-            }
-        }
-        // same func above but for weapon
-        func weapon() -> Weapon {
-            switch self {
-                case .warrior: return Sword()
-                case .mage: return Staff()
-                case .colossus: return IronFist()
-                case .dwarf: return Hammer()
+            // func to sync characters's life
+            func life() -> Int {
+                switch self {
+                    case .warrior: return 600
+                    case .mage: return 800
+                    case .colossus: return 900
+                    case .dwarf: return 400
                 }
             }
-                
-    }
+            // same func above but for weapon
+            func weapon() -> Weapon {
+                switch self {
+                    case .warrior: return Sword()
+                    case .mage: return Staff()
+                    case .colossus: return IronFist()
+                    case .dwarf: return Hammer()
+                }
+            }
+        }
            
     
     
