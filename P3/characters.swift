952b7aa20d0be13b3a1243b670c72a58daa88936
elodie <elodie@ca-rool.com>
2022-05-12 16:31:00 +0200

    func created on enum for better sync with characters

--- P3/characters.swift
+++ P3/characters.swift
@@ -1,21 +1,53 @@
 class Character {
     var name = ""
-    var weapon : Weapon
+    var weapon: Weapon
     var life = 0
+    let type: CharacterType
     
     
-    init(name : String, life : Int, weapon : Weapon) {
+    init(name: String, type: CharacterType) {
         self.name = name
-        self.life = life
-        self.weapon = weapon
+        // weapon enum
+        self.weapon = type.weapon()
+        //life is now combinate with life
+        self.life = type.life()
+        self.type = type
     }
-        
-        func attack( character : Character){
+    
+        func attack(character: Character){
             character.life = character.life - self.weapon.damage
-            print("\(self.name) damaged \(character.name)by \(self.weapon.damage) ! ")
+            print("\(self.name) damaged \(character.name)by \(self.weapon.damage) !")
        
     }
     
+        // so nothing else can be choose
+        enum CharacterType: String {
+                case warrior = "Warrior"
+                case mage = "Mage"
+                case colossus = "Colossus"
+                case dwarf = "Dwarf"
+    
+        // func to sync characters's life
+        func life() -> Int {
+            switch self {
+                case .warrior: return 600
+                case .mage: return 800
+                case .colossus: return 900
+                case .dwarf: return 400
+            }
+        }
+        // same func above but for weapon
+        func weapon() -> Weapon {
+            switch self {
+                case .warrior: return Sword()
+                case .mage: return Staff()
+                case .colossus: return IronFist()
+                case .dwarf: return Hammer()
+                }
+            }
+                
+    }
+           
     
     
 }
