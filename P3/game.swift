bbe13d5df19126dc24a4852ed4cb88425adcb10e
elodie <elodie@ca-rool.com>
2022-05-12 11:16:52 +0200

    game need to be cleaned and make it functional

--- P3/game.swift
+++ P3/game.swift
@@ -16,24 +16,47 @@ class Game {
     var checkWinner: Bool
     var startGame: Bool
     var endGame: Bool
+    
 
     init (players: [Player], teams: [Team], checkWinner: Bool, startGame: Bool, endGame: Bool) {
     
         self.players = players
         self.teams = teams
-        self.checkWinner = checkWinner
-        self.startGame = startGame
-        self.endGame = endGame
+        self.checkWinner = checkWinner()
+        self.startGame = startGame()
+        self.endGame = endGame()
+   
+        // small intro to the game
+        func intro() -> String {
+            return "Welcome to War is love! Two players will fight for their life. Each player will create a team composed of 3 characters. Every player, Team and warrior need a name! It's a turn by turn game, each player will choose a character to attack the other player's character. The winner is the last player with a character alive. Good luck!"
+        }
+        
+        
+        func endGame() -> Bool{
+               if (self.players[0].character[0].life == 0 && self.players[0].character[1].life == 0 && self.players[0].character[2].life == 0) || (self.players[1].character[0].life == 0 && self.players[1].character[1].life == 0 && self.players[1].character[2].life == 0) {
+                   return false
+               }else{
+                   return true
+               }
+           }
+        
+        func checkWinner() -> Player {
+            if (self.players[0].character[0].life == 0 && self.players[0].character[1].life == 0 && self.player[0].character[2].life == 0) {
+                return self.player[1]
+            }else{
+                return self.player[0]
+            }
+        }
+        
     }
-
-    // small intro to the game
-    func intro() -> String {
-        return "Welcome to War is love! Two players will fight for their life. Each player will create a team composed of 3 characters. Every player, Team and warrior need a name! It's a turn by turn game, each player will choose a character to attack the other player's character. The winner is the last player with a character alive. Good luck!" 
-    }
+    
+   
+     
+    
 
     //function to show all characters from class Character
-    func showCharacters() {
-        for character in Characters {
+    public func showCharacters() {
+        for characters in Characters {
             print("\(character.name) is a \(character.type) with \(character.life) life points")
         }
     }
@@ -42,20 +65,25 @@ class Game {
     
     // function to start the game
 
-    func startGame() -> {
-       intro()
-       // ask both players for their name and use func isNameUnique to check if the player2 diferent name from player1
-        print("Player 1, please enter your name")
-        var player1Name = readLine()!
-        print("Player 2, please enter your name")
-        var player2Name = readLine()!
-        if player1Name == player2Name {
-            print("Oups, name already taken")
-            return print("Player 2, please enter a different name then Player 1")
-        }
+ func startGame() -> {
+      
+        intro()
         // showCharacters func to show all characters
         showCharacters()
+       // ask both players for their name and use func isNameUnique to check if the player2 diferent name from player1
+        print("Player 1, please enter your name")
+        let name1 = readLine()
+        print("Player 2, please enter your name")
+        let name2 = readLine()!
+        if name1 == name2 {
+            print("Oups, name already taken")
+            return print("Player 2, please enter a different name then Player 1")
+            let name2 = readLine()!
+        }
+        }
+       
         // ask player1 to choose 3 characters on the characters array
+    func PickCharater() -> String {
         print("Player 1, please choose 3 characters")
         var player1Characters = [Character]()
         for i in 0...2 {
@@ -72,27 +100,13 @@ class Game {
                 player1Characters.append(Dwarf)
             default:
                 print("invalid choice")
+                return ("team completed")
             }
         }
+        
+        print("Player 2, please choose 3 characters")
         showCharacters()
         // ask player2 to choose 3 characters on the characters array
-        print("Player 2, please choose 3 characters")
-        var player2Characters = [Character]()
-        for i in 0...2 {
-            print("choose your character")
-            var choice = readLine()!
-            switch choice {
-            case "1":
-                player2Characters.append(Warrior)
-            case "2":
-                player2Characters.append(Mage)
-            case "3":
-                player2Characters.append(Colossus)
-            case "4":
-                player2Characters.append(Dwarf)
-            default:
-                print("invalid choice")
-            }
-        }
+        PickCharater()
     }
 }
