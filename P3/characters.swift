7e9baab97715c948fba5073e8c17081da152d291
elodie <elodie@ca-rool.com>
2022-05-12 11:17:12 +0200

    character diet, not functional

--- P3/characters.swift
+++ P3/characters.swift
@@ -1,41 +1,21 @@
-//
-//  characters.swift
-//  
-//
-//  Created by Elodie GAGE on 14/04/2022.
-//
-
-import Foundation
-
 class Character {
-    let name: String
-    let type: String
-    var life: Int
-    let lifeMaxLimit: Int
-    var isAlive: Bool
-    var weapon: Weapon
-    func isNameUnique(name: String) -> Bool {
-        if name == Warrior.name || name == Mage.name || name == Colossus.name || name == Dwarf.name {
-            return false
-        } else {
-            return true
+    var name = ""
+    var weapon : Weapon
+    var life = 0
+    init(name : String, life : Int, weapon : Weapon) {
+        self.name = name
+        self.life = life
+        self.weapon = weapon
+        
+        
+        func attack( character : Character){
+            character.life = character.life - self.weapon.damage
+            print("\(self.name) damaged \(character.name)by \(self.weapon.damage) ! ")
         }
-    
+        
+        
+    }
 }
 
-init(type: String, life: Int, weapon: Weapon, name: String, isAlive: Bool) {
-  self.type = type
-  self.life = life
-  self.weapon = weapon
-  self.name = name
-  self.lifeMaxLimit = life
-  self.isAlive = isAlive
-}
-
-
-let Warrior = Character(name: "warrior",type: "Guard", hp: 200, lifeMaxLimit: 200, Weapon:"sword")
-let Mage = Character(name: "Magus", type: "Healer", hp: 300, lifeMaxLimit: 300, weapon: "staff")
-let Colossus = Character(name: "Colossus", type: "Wall", hp: 400, lifeMaxLimit: 400, weapon: "iron fist")
-let Dwarf = Character(name: "Dwarf", hp: 100, type: "Cannon fodder", lifeMaxLimit: 100, weapon: "axe")
-
+ 
 
