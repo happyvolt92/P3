b82d630f55a765ed770a61e0cd799aeddf8c1600
elodie <elodie@ca-rool.com>
2022-05-12 12:17:10 +0200

    intro written

---
+++ P3/weapon/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  P3
+//
+//  Created by Elodie GAGE on 12/05/2022.
+//
+
+import Foundation
+
+
+
+   /// Function to launch the game
+    func StartGame() {
+       
+       let game = Game()
+       
+       game.PlayerNames()
+    
+   
+       print("Welcome to War is love! Two players will fight for their life. Each player will create a team composed of 3 characters. Every player,warrior need a name! It's a turn by turn game, each player will choose a character to attack the other player's character. The winner is the last player with a character alive. Good luck!")
+    }
+
