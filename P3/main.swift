9b2d5c7132feafed6b6c38bacfae794a09ea2fc9
elodie <elodie@ca-rool.com>
2022-06-17 14:13:11 +0200

    deleted print

--- P3/main.swift
+++ P3/main.swift
@@ -16,10 +16,7 @@ import Foundation
    
        game.launchGame()
     
-   // Game explanation on start
-       print("Welcome to War is love! Two players will fight for their life. Each player will create a team composed of 3 characters. Every player,warrior need a name! It's a turn by turn game, each player will choose a character to attack the other player's character. The winner is the last player with a character alive. Good luck!")
     }
 
-    
     StartGame()
    
